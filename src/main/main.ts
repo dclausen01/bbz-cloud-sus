ae563d5b58d870015b6bdd227f4922e38916caa2
koyu <me@koyu.space>
2022-05-03 12:49:04 +0200

    Add download handler

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,12 +1,17 @@
+/* eslint-disable promise/catch-or-return */
+/* eslint-disable no-var */
 /* eslint-disable @typescript-eslint/no-shadow */
 /* eslint-disable vars-on-top */
 /* eslint global-require: off, no-console: off, promise/always-return: off */
 
 import path from 'path';
-import { app, BrowserWindow, shell } from 'electron';
+import { app, BrowserWindow, shell, dialog } from 'electron';
 import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import { resolveHtmlPath } from './util';
+
+var fs = require('fs');
+var https = require('https');
 
 export default class AppUpdater {
   constructor() {
@@ -107,11 +112,72 @@ app.on('window-all-closed', () => {
   }
 });
 
+function download(url, dest) {
+  try {
+    var file = fs.createWriteStream(dest);
+    var request = https
+      .get(url, function (response) {
+        response.pipe(file);
+        file.on('finish', function () {
+          file.close(function () {
+            console.log('file saved.');
+          }); // close() is async, call cb after close completes.
+        });
+      })
+      .on('error', function (e) {
+        // Handle errors
+        fs.unlink(dest); // Delete the file async. (But we don't check the result)
+      });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+const downloadtypes = [
+  '.mp4',
+  '.mp3',
+  '.ogg',
+  '.flac',
+  '.wav',
+  '.mkv',
+  '.mov',
+  '.wmv',
+  '.oga',
+  '.ogv',
+  '.opus',
+  '.pdf',
+  '.xls',
+  '.ppt',
+  '.zip',
+  '.exe',
+  '.AppImage',
+  '.snap',
+  '.bin',
+  '.sh',
+  '.doc',
+];
+
+function isDownloadType(url: string) {
+  var isdt = false;
+  downloadtypes.forEach((s) => {
+    if (url.includes(s)) {
+      isdt = true;
+    }
+  });
+  return isdt;
+}
+
 // Open third-party links in browser
 app.on('web-contents-created', (e, contents) => {
-  if (contents.getType() === 'webview') {
-    // eslint-disable-next-line no-var
-    var handleNewWindow = (e, url) => {
+  // eslint-disable-next-line no-var
+  var handleNewWindow = (e, url) => {
+    if (url.includes('about:blank') || url.includes('download')) {
+      if (url.includes('about:blank')) {
+        e.preventDefault();
+      } else {
+        contents.loadURL(url);
+      }
+    } else if (!isDownloadType(url)) {
       if (!url.includes('onedrive')) {
         e.preventDefault();
         const newWin = new BrowserWindow({
@@ -119,16 +185,41 @@ app.on('web-contents-created', (e, contents) => {
           height: 728,
           minWidth: 600,
           minHeight: 300,
+          show: false,
         });
         newWin.loadURL(url);
         newWin.setMenu(null);
+        if (!url.includes('about:blank') || !url.includes('download')) {
+          newWin.show();
+        }
       } else {
         e.preventDefault();
         contents.loadURL(url);
       }
-    };
-    contents.on('new-window', handleNewWindow);
-  }
+    }
+  };
+  contents.on('new-window', handleNewWindow);
+
+  var handleNavigation = (e, url) => {
+    if (isDownloadType(url)) {
+      e.preventDefault();
+      var toLocalPath = path.resolve(
+        app.getPath('downloads'),
+        path.basename(url)
+      );
+      var userChosenPath = dialog.showSaveDialog({
+        defaultPath: toLocalPath,
+      });
+      if (userChosenPath) {
+        userChosenPath.then((value) => {
+          if (isDownloadType(url)) {
+            download(url, value.filePath);
+          }
+        });
+      }
+    }
+  };
+  contents.on('will-navigate', handleNavigation);
 });
 
 app
